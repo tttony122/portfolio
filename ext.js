e44fe7f9793c810b5e8f33da3861d85280e28086
tttony122 <69151521+tttony122@users.noreply.github.com>
2020-09-20 14:07:03 +0800

    Add files via upload

--- ext.js
+++ ext.js
@@ -2,18 +2,12 @@ $(function(){
 	$(window).on('load resize', function(){
 			var screenSize = $(window).width();	
 			if (screenSize > 1317){
-				
-		
-				
 				$("section#details,section#skill,section#profile").show();
-				
 			} else if (screenSize >= 220 || screenSize <= 1316 ) {
-				
 				$("section#details,section#skill,section#profile").show();
 			} 
 	});
 });
-
 //Mobile Menu
 $(function() {
 	var toggles = document.querySelectorAll(".c-hamburger");
@@ -31,11 +25,9 @@ $(function() {
 			 this.classList.add("is-active");
 			  $("header #mainNav.mobile,header #mainN").css({"display":"block"});
 		}
-
 	  });
 	}
 });
-
 //scrollto
 function scrollto(section){
 	$('html, body').animate({
